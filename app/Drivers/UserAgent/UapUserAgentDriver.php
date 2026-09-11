51af1a7a4969827601e5fe20b7464f8dc7c353a8
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-11 21:41:25 +0200

    Preserve bot company identity

--- app/Drivers/UserAgent/UapUserAgentDriver.php
+++ app/Drivers/UserAgent/UapUserAgentDriver.php
@@ -4,6 +4,7 @@ namespace App\Drivers\UserAgent;
 
 use App\Contracts\UserAgentDriver;
 use App\Enums\Device;
+use App\Normalizers\BotCompanyNormalizer;
 use App\Normalizers\OperatingSystemNormalizer;
 use App\Values\UserAgent;
 use UAParser\Parser;
@@ -18,17 +19,26 @@ final readonly class UapUserAgentDriver implements UserAgentDriver
     {
         $result = $this->parser->parse($userAgent);
         $osFamily = $result->os->family;
-
         $os = OperatingSystemNormalizer::make()->normalize($osFamily);
+        $botCompany = BotCompanyNormalizer::make()->normalize($userAgent);
+        $device = Device::normalize(
+            family: $result->device->family,
+            os: $os,
+            userAgent: $userAgent,
+        );
+
+        if ($botCompany !== null || $device === Device::Bot) {
+            return new UserAgent(
+                browser: $botCompany,
+                os: null,
+                device: Device::Bot,
+            );
+        }
 
         return UserAgent::from(
             browser: $result->ua->family,
             os: $os,
-            device: Device::normalize(
-                family: $result->device->family,
-                os: $os,
-                userAgent: $userAgent
-            )
+            device: $device,
         );
     }
 }
