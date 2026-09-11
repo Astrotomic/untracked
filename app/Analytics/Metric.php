ec05e9db8697a955b11e657c449a6fd818b04901
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-11 13:29:56 +0200

    add attribution metrics

--- app/Analytics/Metric.php
+++ app/Analytics/Metric.php
@@ -10,4 +10,10 @@ enum Metric: string
     case OperatingSystem = 'os';
     case Device = 'device';
     case Format = 'format';
+    case Referrer = 'referrer';
+    case UtmSource = 'utm_source';
+    case UtmMedium = 'utm_medium';
+    case UtmCampaign = 'utm_campaign';
+    case UtmTerm = 'utm_term';
+    case UtmContent = 'utm_content';
 }
