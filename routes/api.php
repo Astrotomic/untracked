97c043fb27923add6b1514ae60ac59ba9447c8a3
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-11 17:51:36 +0200

    validate website origin in middleware

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\ProcessedCollectController;
 use App\Http\Controllers\RawCollectController;
+use App\Http\Middleware\ValidateWebsiteOrigin;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/websites/{website}/collect/raw', RawCollectController::class)
-    ->name('collect.raw');
+Route::middleware(ValidateWebsiteOrigin::class)->group(function (): void {
+    Route::post('/websites/{website}/collect/raw', RawCollectController::class)
+        ->name('collect.raw');
 
-Route::post('/websites/{website}/collect/processed', ProcessedCollectController::class)
-    ->name('collect.processed');
+    Route::post('/websites/{website}/collect/processed', ProcessedCollectController::class)
+        ->name('collect.processed');
+});
