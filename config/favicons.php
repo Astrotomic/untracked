9b3842e7b7adfa0e9ae7633c097e9fe861c09420
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-12 00:49:26 +0200

    Configure favicon drivers

--- config/favicons.php
+++ config/favicons.php
@@ -2,4 +2,10 @@
 
 return [
     'default' => env('FAVICON_DRIVER', 'duckduckgo'),
+
+    'drivers' => [
+        'logo_dev' => [
+            'token' => env('LOGO_DEV_TOKEN'),
+        ],
+    ],
 ];
