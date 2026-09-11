683c511368606507b9d196ee11946c9bbc7f1fb0
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-12 00:36:32 +0200

    Remove old top-level favicon view

--- resources/views/components/favicon.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($url !== null)
-    <img
-        src="{{ $url }}"
-        alt=""
-        loading="lazy"
-        referrerpolicy="no-referrer"
-        {{ $attributes->class(['size-4 shrink-0 rounded-sm']) }}
-    >
-@endif
