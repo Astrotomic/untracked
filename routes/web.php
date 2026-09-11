4f0ffcf1dd3e7316036c484c71717375b3ee7246
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-11 23:16:01 +0200

    Register analytics test endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\TestController;
 use App\Http\Controllers\WebsiteController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/test', TestController::class)->name('test');
 
 Route::middleware('guest')->group(function (): void {
     Route::get('/login', [LoginController::class, 'create'])->name('login');
