0543609cf92cd6949b04885a22cc6669ffaf2f33
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-11 12:57:17 +0200

    split raw and processed collection endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\CollectController;
+use App\Http\Controllers\ProcessedCollectController;
+use App\Http\Controllers\RawCollectController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/websites/{website}/collect', CollectController::class)
-    ->name('collect');
+Route::post('/websites/{website}/collect/raw', RawCollectController::class)
+    ->name('collect.raw');
+
+Route::post('/websites/{website}/collect/processed', ProcessedCollectController::class)
+    ->name('collect.processed');
