5a99dbf5aa1429d139a4b6f529a230b33ed945b5
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-11 21:41:19 +0200

    Allow nullable user agent dimensions

--- app/Values/UserAgent.php
+++ app/Values/UserAgent.php
@@ -8,10 +8,10 @@ use App\Normalizers\OperatingSystemNormalizer;
 
 final readonly class UserAgent
 {
-    public static function from(string $browser, string $os, string|Device $device): self
+    public static function from(?string $browser, ?string $os, string|Device $device): self
     {
-        $browser = BrowserNormalizer::make()->normalize($browser);
-        $os = OperatingSystemNormalizer::make()->normalize($os);
+        $browser = $browser === null ? null : BrowserNormalizer::make()->normalize($browser);
+        $os = $os === null ? null : OperatingSystemNormalizer::make()->normalize($os);
         $device = is_string($device) ? Device::from($device) : $device;
 
         return new self(
@@ -22,15 +22,13 @@ final readonly class UserAgent
     }
 
     public function __construct(
-        public string $browser,
-        public string $os,
+        public ?string $browser,
+        public ?string $os,
         public Device $device,
     ) {}
 
     public function isBot(): bool
     {
-        return strcasecmp($this->browser, 'bot') === 0
-            || strcasecmp($this->os, 'bot') === 0
-            || $this->device === Device::Bot;
+        return $this->device === Device::Bot;
     }
 }
