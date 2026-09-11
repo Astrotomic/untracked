a3610dd5e6660cef2593adecb4dab69d514adc8c
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-11 12:55:01 +0200

    add analytics enums and resolver drivers

---
+++ app/Analytics/Enums/OperatingSystem.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Analytics\Enums;
+
+enum OperatingSystem: string
+{
+    case Windows = 'Windows';
+    case MacOS = 'macOS';
+    case Linux = 'Linux';
+    case Android = 'Android';
+    case IOS = 'iOS';
+    case ChromeOS = 'ChromeOS';
+    case Bot = 'Bot';
+    case Unknown = 'Unknown';
+    case Other = 'Other';
+
+    public static function normalize(string $family): self
+    {
+        $family = strtolower(trim($family));
+
+        return match (true) {
+            $family === '', $family === 'other' => self::Unknown,
+            str_contains($family, 'windows') => self::Windows,
+            str_contains($family, 'mac os'), str_contains($family, 'macos'), str_contains($family, 'os x') => self::MacOS,
+            str_contains($family, 'android') => self::Android,
+            str_contains($family, 'ios'), str_contains($family, 'iphone os'), str_contains($family, 'ipad os') => self::IOS,
+            str_contains($family, 'chrome os'), str_contains($family, 'cros') => self::ChromeOS,
+            str_contains($family, 'linux') => self::Linux,
+            default => self::Other,
+        };
+    }
+}
