70a4d2cf20cf2315a1aa6d8f43ac31a5cb64f911
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-12 00:49:34 +0200

    Add Google favicon driver

---
+++ app/Drivers/Favicon/GoogleFaviconDriver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Drivers\Favicon;
+
+use App\Contracts\FaviconDriver;
+
+final class GoogleFaviconDriver implements FaviconDriver
+{
+    public function url(string $domain, ?int $size = null): string
+    {
+        return 'https://t1.gstatic.com/faviconV2?'.http_build_query([
+            'client' => 'SOCIAL',
+            'type' => 'FAVICON',
+            'fallback_opts' => 'TYPE,SIZE,URL',
+            'url' => 'https://'.$domain,
+            'size' => $size ?? 128,
+        ], '', '&', PHP_QUERY_RFC3986);
+    }
+}
