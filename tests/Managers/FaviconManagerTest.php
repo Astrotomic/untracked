7ddc5de2040a0db3b86d11636b061a0a3e03ab22
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-12 00:18:09 +0200

    Keep favicon manager test manager-only

--- tests/Managers/FaviconManagerTest.php
+++ tests/Managers/FaviconManagerTest.php
@@ -2,7 +2,8 @@
 
 namespace Tests\Managers;
 
-use App\Managers\Favicons\FaviconManager;
+use App\Drivers\Favicon\DuckDuckGoFaviconDriver;
+use App\Managers\FaviconManager;
 use PHPUnit\Framework\Assert;
 use Tests\TestCase;
 
@@ -10,24 +11,9 @@ class FaviconManagerTest extends TestCase
 {
     public function test_it_uses_duckduckgo_by_default(): void
     {
-        Assert::assertSame(
-            'https://icons.duckduckgo.com/ip3/google.com.ico',
-            FaviconManager::make()->url('google.com'),
+        Assert::assertInstanceOf(
+            DuckDuckGoFaviconDriver::class,
+            FaviconManager::make()->driver(),
         );
-    }
-
-    public function test_it_resolves_known_client_domains(): void
-    {
-        Assert::assertSame(
-            'https://icons.duckduckgo.com/ip3/openai.com.ico',
-            FaviconManager::make()->clientUrl('OpenAI'),
-        );
-
-        Assert::assertSame(
-            'https://icons.duckduckgo.com/ip3/firefox.com.ico',
-            FaviconManager::make()->clientUrl('Firefox'),
-        );
-
-        Assert::assertNull(FaviconManager::make()->clientUrl('Ladybird'));
     }
 }
