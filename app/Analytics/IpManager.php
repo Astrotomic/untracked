83560b986d8729c7b2c2b7d80dbaa42f2946184b
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-11 12:56:10 +0200

    add analytics enums and resolver drivers

---
+++ app/Analytics/IpManager.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Analytics;
+
+use App\Analytics\Drivers\IpApiDriver;
+use App\Analytics\Drivers\MaxMindIpDriver;
+use GeoIp2\Database\Reader;
+use Illuminate\Support\Manager;
+
+class IpManager extends Manager
+{
+    public function getDefaultDriver(): string
+    {
+        return (string) config('analytics.ip.driver', 'maxmind');
+    }
+
+    protected function createMaxmindDriver(): MaxMindIpDriver
+    {
+        return new MaxMindIpDriver((string) config('analytics.ip.maxmind.database'));
+    }
+
+    protected function createIpApiDriver(): IpApiDriver
+    {
+        return new IpApiDriver(
+            (string) config('analytics.ip.ip-api.url'),
+            (int) config('analytics.ip.ip-api.timeout', 2),
+        );
+    }
+}
